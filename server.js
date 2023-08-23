6294549b2ebfdf12d858468b67ec17f4fa921113
Ellis Yoo <ellisyoo@elliss-mbp.lan>
2023-08-23 14:17:12 -0700

    Create DB and separate rooms for different documents

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const mongoose = require("mongoose")
+const Document = require("./Document")
+require('dotenv').config()
+
+const db_username = process.env.DB_USERNAME
+const db_pw = process.env.DB_PW
+const db_cluster = process.env.DB_CLUSTER
+
+mongoose.connect(`mongodb+srv://${db_username}:${db_pw}@${db_cluster}.mongodb.net/`)
+    .then(()=>console.log("Connected to database!"))
+
+const defaultValue = ""
+
 const io = require('socket.io')(3001, {
     cors: {
         origin: 'http://localhost:3000',
@@ -6,8 +19,26 @@ const io = require('socket.io')(3001, {
 })
 
 io.on("connection", socket => {
-    socket.on('send-changes', delta => {
-        socket.broadcast.emit("receive-changes", delta)
+    socket.on('get-document', async documentId => {
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId)
+        socket.emit('load-document', document.data)
+
+        socket.on('send-changes', delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta)
+        })
+
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, { data })
+        })
     })
-    // console.log("connected")
 })
+
+async function findOrCreateDocument(id) {
+    if (id === null) return
+
+    const document = await Document.findById(id)
+    if (document) return document
+    return await Document.create({ _id: id, data: defaultValue })
+
+}
