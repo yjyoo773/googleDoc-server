e01a8e3090da2ee92c6f41c22ce3133568aca85e
Ellis Yoo <ellisyoo@elliss-mbp.lan>
2023-08-29 11:32:44 -0700

    Create user model and endpoint to login/create user

--- server.js
+++ server.js
@@ -1,19 +1,113 @@
+const express = require("express")
 const mongoose = require("mongoose")
-const Document = require("./Document")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 require('dotenv').config()
 
-const db_username = process.env.DB_USERNAME
-const db_pw = process.env.DB_PW
-const db_cluster = process.env.DB_CLUSTER
+const User = require("./models/User")
 
-mongoose.connect(`mongodb+srv://${db_username}:${db_pw}@${db_cluster}.mongodb.net/`)
-    .then(()=>console.log("Connected to database!"))
+const Document = require("./models/Document")
+
+// ENV VARIABLES
+
+const PORT = process.env.PORT || 3001
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000"
+
+const app = express()
+app.use(express.json())
+
+// User Create Endpoint
+app.post("/register", (req, res) => {
+    // hash password
+    bcrypt
+    .hash(req.body.password, 10)
+    .then((hashedPW) => {
+        // create a new user instance and collect data
+        const user = new User({
+            email: req.body.email,
+            password: hashedPW,
+        })
+
+        // save new user
+        user.save()
+        .then((result) =>{
+            res.status(201).send({
+                message: `User ${req.body.email} created successfully`,
+                result,
+            })
+        })
+        .catch((error) => {
+            res.status(500).send({
+                message: "Error creating user",
+                error,
+            })
+        })
+    })
+    .catch((error) => {
+        res.status(500).send({
+            message: "Password was not hashed successfully",
+            error,
+        })
+    })
+})
+
+// Login Endpoint
+app.post('/login', (req, res) => {
+    // check if email exists
+    User.findOne({email: req.body.email})
+    .then((user) => {
+        bcrypt.compare(req.body.password, user.password)
+        .then((passwordCheck) => {
+            if(!passwordCheck) {
+                return res.status(400).send({
+                    message: "Passwords do not match",
+                    error,
+                })
+            }
+
+            // create JWT Token
+            const token = jwt.sign(
+                {
+                    userId: user._id,
+                    userEmail: user.email,
+                },
+                "RANDOM-TOKEN",
+                {expiresIn: "24h"}
+            )
+
+            // return success response
+            res.status(200).send({
+                message: "Login successful",
+                email: user.email,
+                token,
+            })
+        })
+        .catch((error) => {
+            res.status(400).send({
+                message: "Passwords do not match",
+                error
+            })
+        })
+    })
+    // Catch error if email does not exist
+    .catch((error) => {
+        res.status(404).send({
+            message: "Email not found",
+            error
+        })
+    })
+})
+
+
+const dbConnect = require("./db/dbConnect");
+dbConnect()
+
 
 const defaultValue = ""
 
-const io = require('socket.io')(3001, {
+const io = require('socket.io')(PORT, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: CLIENT_URL,
         methods: ['GET', 'POST']
     }
 })
@@ -42,3 +136,8 @@ async function findOrCreateDocument(id) {
     return await Document.create({ _id: id, data: defaultValue })
 
 }
+
+// const PORT = process.env.PORT || 5000;
+app.listen(5050, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
